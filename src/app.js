17a576fa171b11cc044cd9cffa47dc113b22ea14
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-05-27 03:24:21 +0000

    card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'K', 'Q', 'J', 'A']
+let icons = [
+    {key: '♦', color: 'red'},
+    {key: '♥', color: 'red'},
+    {key: '♠', color: 'black'},
+    {key: '♣', color: 'black'}
+] 
+    
+const iconTop = document.querySelector('.top-icon')
+const iconBottom = document.querySelector('.bottom-icon')
+const cardValue = document.querySelector('.number')
+const btnGenerator = document.querySelector('.generate')
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+btnGenerator.addEventListener('click', generateCard)
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function generateCard (){
+    let randomNum = numbers[Math.floor(Math.random() * numbers.length)]
+    let randomIcon = icons[Math.floor(Math.random() * icons.length)]
+
+    iconTop.innerHTML = randomIcon.key
+    iconBottom.innerHTML = randomIcon.key
+    iconTop.style.color = randomIcon.color
+    iconBottom.style.color = randomIcon.color
+    cardValue.innerHTML = randomNum
+}
+
+
