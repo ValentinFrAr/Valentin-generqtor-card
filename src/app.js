d2c2bbeae0bd00c6dde03f8b0117afe75e20058a
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-05-27 03:36:26 +0000

    card

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'K', 'Q', 'J', 'A']
+import "./style.css";
+
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
 let icons = [
-    {key: '♦', color: 'red'},
-    {key: '♥', color: 'red'},
-    {key: '♠', color: 'black'},
-    {key: '♣', color: 'black'}
-] 
-    
-const iconTop = document.querySelector('.top-icon')
-const iconBottom = document.querySelector('.bottom-icon')
-const cardValue = document.querySelector('.number')
-const btnGenerator = document.querySelector('.generate')
+  { key: "♦", color: "red" },
+  { key: "♥", color: "red" },
+  { key: "♠", color: "black" },
+  { key: "♣", color: "black" }
+];
 
-btnGenerator.addEventListener('click', generateCard)
+const iconTop = document.querySelector(".top-icon");
+const iconBottom = document.querySelector(".bottom-icon");
+const cardValue = document.querySelector(".number");
+const btnGenerator = document.querySelector(".generate");
 
+btnGenerator.addEventListener("click", generateCard);
 
-function generateCard (){
-    let randomNum = numbers[Math.floor(Math.random() * numbers.length)]
-    let randomIcon = icons[Math.floor(Math.random() * icons.length)]
+function generateCard() {
+  let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
 
-    iconTop.innerHTML = randomIcon.key
-    iconBottom.innerHTML = randomIcon.key
-    iconTop.style.color = randomIcon.color
-    iconBottom.style.color = randomIcon.color
-    cardValue.innerHTML = randomNum
+  iconTop.innerHTML = randomIcon.key;
+  iconBottom.innerHTML = randomIcon.key;
+  iconTop.style.color = randomIcon.color;
+  iconBottom.style.color = randomIcon.color;
+  cardValue.innerHTML = randomNum;
 }
-
-
